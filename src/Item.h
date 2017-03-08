1186cad4a9d6478b74cc59e6a9f08ff76e2bee35
denesik <denesic@gmail.com>
2017-03-08 19:28:50 +0300

    item

---
+++ src/Item.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef Item_h__
+#define Item_h__
+
+#include <stdint.h>
+
+typedef uint32_t ItemId;
+
+class Item
+{
+public:
+  Item();
+  ~Item();
+
+
+};
+
+
+
+#endif // Item_h__
