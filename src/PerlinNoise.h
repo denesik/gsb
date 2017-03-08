5b39d3505d54ebc9615e2f7d70aa9d3f2ce08acb
denesik <denesic@gmail.com>
2017-03-08 17:30:54 +0300

    fix define

--- src/PerlinNoise.h
+++ src/PerlinNoise.h
@@ -2,22 +2,7 @@
 #ifndef PerlinNoise_h__
 #define PerlinNoise_h__
 
-#define B 0x100
-#define BM 0xff
-#define N 0x1000
-#define NP 12   /* 2^N */
-#define NM 0xfff
 
-#define s_curve(t) ( t * t * (3. - 2. * t) )
-#define lerp(t, a, b) ( a + t * (b - a) )
-#define setup(i,b0,b1,r0,r1)\
-        t = vec[i] + N;\
-        b0 = ((int)t) & BM;\
-        b1 = (b0+1) & BM;\
-        r0 = t - (int)t;\
-        r1 = r0 - 1.;
-#define at2(rx,ry) ( rx * q[0] + ry * q[1] )
-#define at3(rx,ry,rz) ( rx * q[0] + ry * q[1] + rz * q[2] )
 
 void init(void);
 double noise1(double);
