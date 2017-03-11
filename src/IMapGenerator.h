1788cc4495aafa919af5a0f65800c3d832bc6d83
denesik <denesic@gmail.com>
2017-03-11 20:01:52 +0300

    fix compile err

--- src/IMapGenerator.h
+++ src/IMapGenerator.h
@@ -11,10 +11,10 @@ public:
   virtual ~IMapGenerator() = default;
   virtual void Generate(Sector &sector) = 0;
   
-  void UpdateGui(const Magnum::Timeline &);
-  template<typename T>
-  void AddParameter(const T & parameter, const std::string & name, const std::string & description = "");
-  void AddCustomParameter(std::function<void(Magnum::Timeline)> gui);
+//   void UpdateGui(const Magnum::Timeline &);
+//   template<typename T>
+//   void AddParameter(const T & parameter, const std::string & name, const std::string & description = "");
+//   void AddCustomParameter(std::function<void(Magnum::Timeline)> gui);
 protected:
   const BlocksDataBase &m_Db;
 };
