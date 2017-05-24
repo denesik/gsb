957da4feaa25775b648309718a5fd721696f30e3
ishellstrike <ishellstrike@gmail.com>
2017-05-25 01:25:06 +0300

    rb partial fix

--- src/RingBuffer.h
+++ src/RingBuffer.h
@@ -8,140 +8,17 @@
 
 namespace
 {
-  inline void StoreOrderedInc(SPos & pos, Magnum::Vector2i radius)
+  inline void StoreOrderedInc(Magnum::Vector2i & pos, Magnum::Vector2i radius)
   {
-    pos.z()++;
-    if (pos.z() > radius.y())
+    pos.y()++;
+    if (pos.y() > radius.y())
     {
-      pos.z() = -radius.y();
+      pos.y() = -radius.y();
       pos.x()++;
     }
   }
 }
-/*
 
-template<typename T>
-class RingBuffer2
-{
-public:
-  using StoredType = T;
-  using AddFunction = std::function<T(const SPos &)>;
-  using DeleteFunction = std::function<void(T &, const SPos &)>;
-
-  RingBuffer2<T>(AddFunction add, DeleteFunction del)
-    : onAdding(add)
-    , onDeletting(del)
-  {
-    //SetSize(size);
-  }
-
-  RingBuffer2<T> &SetSize(Magnum::Vector2i size)
-  {
-    if (!mStorage.empty())
-    {
-      SPos unoffseted = { -mRadius.x(), 0, -mRadius.y() };
-      for (int i = 0; i < mDim.x() * mDim.y(); i++)
-      {
-        SPos spos = mPos + unoffseted;
-        onDeletting(mStorage[i], spos);
-
-        StoreOrderedInc(unoffseted, mRadius);
-      }
-    }
-
-    mRadius = size;
-    mDim = size * 2 + Magnum::Vector2i(1, 1);
-    mStorage.clear();
-
-    SPos unoffseted = { -mRadius.x(), 0, -mRadius.y() };
-    for (int i = 0; i < mDim.x() * mDim.y(); i++)
-    {
-      SPos spos = mPos + unoffseted;
-
-      mStorage.emplace_back(onAdding(spos));
-
-      StoreOrderedInc(unoffseted, mRadius);
-    }
-
-    mBias = mPos;
-
-    return *this;
-  }
-
-  boost::optional<T &> Get(const SPos &spos)
-  {
-    if ((std::abs(mPos.x() - spos.x()) > mRadius.x()) || (std::abs(mPos.z() - spos.z()) > mRadius.y()))
-      return{};
-
-    Magnum::Vector2i sec_pos = WrapSPos(Magnum::Vector2i{ spos.x(), spos.z() });
-    Magnum::Vector2i wpos = Magnum::Vector2i(spos.x(), spos.z()) - sec_pos * mDim + mRadius;
-    auto t = wpos.x() * mDim.x() + wpos.y();
-    return mStorage[t];
-  }
-
-  template <typename T> int sgn(T val) {
-    return (T(0) < val) - (val < T(0));
-  }
-
-  //TODO: тестить на больших дельтах
-  void UpdatePos(const SPos &pos)
-  {
-    if (pos == mPos)
-      return;
-
-    SPos unoffseted = { -mRadius.x(), 0, -mRadius.y() };
-    for (int i = 0; i < mDim.x() * mDim.y(); i++)
-    {
-      SPos oldPos = mPos + unoffseted;
-      SPos newPos = pos + unoffseted;
-      if (std::abs(oldPos.x() - pos.x()) > mRadius.x() || std::abs(oldPos.z() - pos.z()) > mRadius.y())
-      {
-        onDeletting(mStorage[i], oldPos);
-        mStorage[i] = onAdding(newPos);
-      }
-
-      StoreOrderedInc(unoffseted, mRadius);
-    }
-
-    mPos = pos;
-  }
-
-  const SPos &GetPos()
-  {
-    return mPos;
-  }
-
-  auto begin()
-  {
-    return mStorage.begin();
-  }
-
-  auto end()
-  {
-    return mStorage.end();
-  }
-
-  AddFunction onAdding;
-  DeleteFunction onDeletting;
-
-private:
-  inline Magnum::Vector2i WrapSPos(const Magnum::Vector2i &_pos)
-  {
-    const Magnum::Vector2i pos = _pos - Magnum::Vector2i(mBias.x(), mBias.z()) + mRadius;
-    Magnum::Vector2i spos;
-
-    spos.x() = (pos.x() >= 0) ? static_cast<int>(pos.x()) / mDim.x() : (static_cast<int>(pos.x()) - mDim.x() + int(1)) / mDim.x();
-    spos.y() = (pos.y() >= 0) ? static_cast<int>(pos.y()) / mDim.y() : (static_cast<int>(pos.y()) - mDim.y() + int(1)) / mDim.y();
-
-    return spos;
-  }
-
-  std::vector<T> mStorage;
-  Magnum::Vector2i mRadius;
-  Magnum::Vector2i mDim;
-  SPos mPos;
-  SPos mBias;
-};*/
 
 template<typename T>
 class RingBuffer
@@ -155,43 +32,103 @@ public:
     : onAdding(add)
     , onDeletting(del)
   {
+    //SetSize(size);
   }
 
   RingBuffer<T> &SetSize(Magnum::Vector2i size)
   {
-    if (size == mSize)
-      return *this;
-    mSize = size;
-    Update();
+    if (!mStorage.empty())
+    {
+      Magnum::Vector2i unoffseted = -mRadius;
+      for (int i = 0; i < mDim.x() * mDim.y(); i++)
+      {
+        Magnum::Vector2i spos = ((ItoPos(i) - mOffset) % mRadius) + mCenter;
+        
+        onDeletting(mStorage[i], SPos(spos.x(), 0, spos.y()));
+
+        StoreOrderedInc(unoffseted, mRadius);
+      }
+    }
+
+    mRadius = size;
+    mDim = size * 2 + Magnum::Vector2i(1, 1);
+    mStorage.clear();
+
+    Magnum::Vector2i unoffseted = -mRadius;
+    for (int i = 0; i < mDim.x() * mDim.y(); i++)
+    {
+      Magnum::Vector2i spos = mCenter + unoffseted;
+
+      mStorage.emplace_back(onAdding(SPos(spos.x(), 0, spos.y())));
+
+      StoreOrderedInc(unoffseted, mRadius);
+    }
+
+    mOffset = {};
+
     return *this;
   }
 
-  boost::optional<T &> Get(const SPos &spos)
+  boost::optional<T &> Get(const SPos & _spos)
   {
-    auto it = std::find_if(mPositions.begin(), mPositions.end(), [&spos](const SPos &pos)
-    {
-      return pos == spos;
-    });
+    auto spos = Magnum::Vector2i(_spos.x(), _spos.z());
+    if ((std::abs(mCenter.x() - spos.x()) > mRadius.x()) || (std::abs(mCenter.y() - spos.y()) > mRadius.y()))
+      return{};
 
-    if (it != mPositions.end())
-    {
-      return mStorage[std::distance(mPositions.begin(), it)];
-    }
-
-    return{};
+    auto locPos = spos - mCenter + mOffset;
+    auto wraped = WrapSPos(locPos);
+    auto t = PosToIndex(wraped);
+    return mStorage[t];
   }
 
-  void UpdatePos(const SPos &pos)
+  template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+  }
+
+  //TODO: тестить на больших дельтах
+  void UpdatePos(const SPos & _newCenter)
   {
-    if (pos == mPos)
+    auto newCenter = Magnum::Vector2i(_newCenter.x(), _newCenter.z());
+
+    if (newCenter == mCenter)
       return;
-    mPos = pos;
-    Update();
+
+    mOffset += mCenter - newCenter;
+
+    Magnum::Vector2i unoffseted = -mRadius;
+    for (int i = 0; i < mDim.x() * mDim.y(); i++)
+    {
+      Magnum::Vector2i oldPos = mCenter + unoffseted;
+      if (std::abs(oldPos.x() - newCenter.x()) > mRadius.x() || std::abs(oldPos.y() - newCenter.y()) > mRadius.y())
+      {
+        auto wraped = WrapSPos(oldPos);
+        auto t = PosToIndex(wraped);
+        onDeletting(mStorage[t], SPos(oldPos.x(), 0, oldPos.y()));
+      }
+
+      StoreOrderedInc(unoffseted, mRadius);
+    }
+
+    unoffseted = -mRadius;
+    for (int i = 0; i < mDim.x() * mDim.y(); i++)
+    {
+      Magnum::Vector2i newPos = newCenter + unoffseted;
+      if (std::abs(newPos.x() - mCenter.x()) > mRadius.x() || std::abs(newPos.y() - mCenter.y()) > mRadius.y())
+      {
+        auto wraped = WrapSPos(newPos);
+        auto t = PosToIndex(wraped);
+        mStorage[t] = onAdding(SPos(newPos.x(), 0, newPos.y()));
+      }
+
+      StoreOrderedInc(unoffseted, mRadius);
+    }
+
+    mCenter = newCenter;
   }
 
   const SPos &GetPos()
   {
-    return mPos;
+    return{ mCenter.x(), 0, mCenter.y() };
   }
 
   auto begin()
@@ -208,93 +145,35 @@ public:
   DeleteFunction onDeletting;
 
 private:
+  Magnum::Vector2i WrapSPos(const SPos &val)
+  {
+    return WrapSPos({ val.x(), val.z() });
+  }
+
+  Magnum::Vector2i WrapSPos(const Magnum::Vector2i &val)
+  {
+    Magnum::Vector2i spos;
+
+    spos.x() = (val.x() > mRadius.x()) ? ((val.x() + mRadius.x()) % (mRadius.x() * 2 + 1)) - mRadius.x() : ((val.x() - mRadius.x()) % (mRadius.x() * 2 + 1)) + mRadius.x();
+    spos.y() = (val.y() > mRadius.y()) ? ((val.y() + mRadius.y()) % (mRadius.y() * 2 + 1)) - mRadius.y() : ((val.y() - mRadius.y()) % (mRadius.y() * 2 + 1)) + mRadius.y();
+
+    return spos;
+  }
+
+  Magnum::Vector2i ItoPos(int i)
+  {
+    return { i % (mRadius.x() * 2 + 1) - mRadius.x(), (i / (mRadius.y() * 2 + 1)) % (mRadius.y() * 2 + 1) - mRadius.y() };
+  }
+
+  int PosToIndex(const Magnum::Vector2i &val)
+  {
+    auto valOff = val + mRadius;
+    return valOff.x() * mDim.y() + valOff.y();
+  }
+
   std::vector<T> mStorage;
-  std::vector<SPos> mPositions;
-
-  Magnum::Vector2i mSize;
-  SPos mPos;
-
-private:
-  std::vector<SPos> Generate(const SPos &pos, const Magnum::Vector2i &size)
-  {
-    std::vector<SPos> data;
-    for (int x = -size.x(); x <= size.x(); ++x)
-      for (int y = -size.y(); y <= size.y(); ++y)
-      {
-        data.emplace_back(pos + SPos{ x, 0, y });
-      }
-
-    return data;
-  }
-
-  std::vector<SPos> Association(const std::vector<SPos> &a, const std::vector<SPos> &b)
-  {
-    std::vector<SPos> data;
-    for (const auto &pos : a)
-    {
-      auto it = std::find_if(b.begin(), b.end(), [&pos](const SPos &val)
-      {
-        return val == pos;
-      });
-
-      if (it != b.end())
-      {
-        data.emplace_back(pos);
-      }
-    }
-    return data;
-  }
-
-  std::vector<SPos> Subtraction(const std::vector<SPos> &a, const std::vector<SPos> &b)
-  {
-    std::vector<SPos> data;
-    for (const auto &pos : a)
-    {
-      auto it = std::find_if(b.begin(), b.end(), [&pos](const SPos &val)
-      {
-        return val == pos;
-      });
-
-      if (it == b.end())
-      {
-        data.emplace_back(pos);
-      }
-    }
-    return data;
-  }
-
-  void Update()
-  {
-    auto new_data = Generate(mPos, mSize);
-    auto &old_data = mPositions;
-    auto delta = Association(old_data, new_data);
-    auto old_delta = Subtraction(old_data, delta);
-    auto new_delta = Subtraction(new_data, delta);
-
-    for (const auto &pos : old_delta)
-    {
-      auto it = std::find_if(mPositions.begin(), mPositions.end(), [&pos](const SPos &spos)
-      {
-        return pos == spos;
-      });
-      auto index = std::distance(mPositions.begin(), it);
-      onDeletting(mStorage[index], mPositions[index]);
-      std::swap(mStorage[index], mStorage.back());
-      mStorage.pop_back();
-      std::swap(mPositions[index], mPositions.back());
-      mPositions.pop_back();
-    }
-
-    for (const auto &pos : new_delta)
-    {
-      //       auto it = std::find_if(mPositions.begin(), mPositions.end(), [&pos](const SPos &spos)
-      //       {
-      //         return pos == spos;
-      //       });
-      //       auto index = std::distance(mPositions.begin(), it);
-      mPositions.emplace_back(pos);
-      mStorage.emplace_back(onAdding(pos));
-    }
-  }
-
+  Magnum::Vector2i mRadius;
+  Magnum::Vector2i mDim;
+  Magnum::Vector2i mCenter;
+  Magnum::Vector2i mOffset;
 };
